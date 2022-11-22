337e85df46ec1063f7ea2ff8edcdb67cedf99320
Shreeya Adhikari <shreeya.4708@kistcollege.edu.np>
2022-11-22 20:28:09 +0545

    bootstrap installed in app

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+
+<div class="container">
+<h1>Welcome to Dashboard</h1>
+
+</div>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social media App</title>
+
+    <!-- import gareko -->
+    @vite(['resources/js/app.js'])
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">SocialMedia</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="/">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="post">Posts</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login">Login</a>
+        </li>
+        
+          
+    </div>
+  </div>
+</nav>
+    <!-- dynamic content -->
+    <!-- content naam matra deko like div ma class deko jasto -->
+    @yield('content')
+</body>
+</html>
